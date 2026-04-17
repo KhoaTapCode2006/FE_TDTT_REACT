d28870d40391486df45c849d7edfa94586b9c081
Anh Khoa <khoaanhcoder@gmail.com>
2026-04-17 20:31:12 +0700

    init files

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,10 +1,9 @@
 import { Routes, Route, createBrowserRouter } from "react-router-dom";
-import { LoginPage } from "@/pages/LoginPage";
+
 
 export default function AppRouter() {
   return (
     <Routes>
-      <Route path="/login" element={<LoginPage />} />
     </Routes>
   );
 }
--- src/features/auth/index.js
+++
@@ -1,3 +0,0 @@
-// Chỉ cho phép bên ngoài truy cập vào những thứ cần thiết
-export { LoginFeature } from "./pages/Login";
-export { useAuthStore } from "./auth.store";
